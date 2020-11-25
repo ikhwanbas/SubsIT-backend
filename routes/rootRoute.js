95da6a6c99ebe590b1948677bbe9abaffc2c34ea
Jul <meetredmist@gmail.com>
2020-11-25 14:38:39 +0700

    adjustment

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express.Router()
-const db = require('../controller/dbController')
+const db = require('../controllers/dbController')
 
 app.get('/', (req, res) => {
   res.send("Hello world!")
