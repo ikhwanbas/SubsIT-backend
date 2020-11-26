9761d7194d4cb7d99818f1c16130186b483046cb
Jul <meetredmist@gmail.com>
2020-11-26 18:31:59 +0700

    init

--- routes/rootRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express.Router()
-const db = require('../controllers/dbController')
-
-app.get('/', (req, res) => {
-  res.send("Hello world!")
-})
-
-module.exports = app
