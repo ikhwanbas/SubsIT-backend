f4e479779a0658588da3644dee4b07a29e0ac0a4
Julhend <meetredmist@gmail.com>
2020-12-01 23:33:36 +0700

    .

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@ module.exports = {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB_DATABASE,
-    // "host": "localhost",
-    "host": DB_HOST,
+    "host": "localhost",
+    // "host": DB_HOST,
     "dialect": "mysql",
     //biar ga kotor syntax mysql di terminal
     logging: false
@@ -17,8 +17,8 @@ module.exports = {
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB_DATABASE,
-    "host": DB_HOST,
-    //  "host": "localhost",
+    // "host": DB_HOST,
+    "host": "localhost",
     "dialect": "mysql"
   }
 }
