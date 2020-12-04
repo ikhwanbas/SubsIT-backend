31c53cc4a5df2b6d3c2a8bd9f337cf08b5bd9f76
Julhend <meetredmist@gmail.com>
2020-12-04 13:28:06 +0700

    user

--- routes/authenticate/deleteUSer.js
+++ routes/authenticate/deleteUSer.js
@@ -5,14 +5,26 @@ const mysqlErrorHandler = require('../../middleware/errorMiddleware')
 const passport = require('../../middleware/authorizationMiddleware')
 
 app.delete('/auth/delete', passport.authenticate('bearer', { session: false }), async (req, res, next) => {
-    await db.users.update({
-        status: 'inactive'
-    },
-        {
-            where: { id: req.user.id }
-        })
-        .catch((err) => next(err))
-    res.send('user deleted')
+    let id = req.user.id
+    let user = await db.users.findAll({
+        where: {
+            id: id,
+            status: 'active'
+        }
+    })
+    if (user.length) {
+        await db.users.update({
+            status: 'inactive'
+        },
+            {
+                where: { id: id }
+            })
+            .catch((err) => next(err))
+        res.send('user deleted')
+    } else {
+        res.send("user not found,login pls")
+    }
+
 })
 
 app.use(mysqlErrorHandler)
